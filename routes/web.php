76b8cea519c274d19d2b67dbecf326cbdca7a5f7
Ariel09 <riel.lagata08@gmail.com>
2024-07-26 14:59:07 +0800

    Code Adjustment

--- routes/web.php
+++ routes/web.php
@@ -2,26 +2,22 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('/admin/users')->name('admin.')->group(function () {
-    Route::get('/management', function () {
-        return '<h1 style="color: red">Hello World</h1>';
-    })->name('users.management');
-
-    Route::get('/reports', function () {
-        return '<h1 style="color: red">Hello World</h1>';
-    })->name('users.reports');
+Route::get('/', function () {
+    return view('welcome');
 });
 
-// Route::get('/admin/users/management', function () {
-//     return '<h1 style="color: red">Hello World</h1>';
-// })->name('admin.users.management');
+Route::prefix('user')->name('user.')->group(function(){
+    Route::get('/profile', function(){
+        return '<h3>Name:</h3><h2>Ariel Lagata</h2><br>
+                <h3>address:</h3><h2>Gubat Sorsogon</h2><br>
+                <h3>email:</h3><h2>my@email.com</h2><br>';
+    })->name('profile');
 
-// Route::get('/admin/users/reports', function () {
-//     return '<h1 style="color: red">Hello World</h1>';
-// })->name('admin.users.reports');
+    Route::get('/work-experience', function(){
+        return '<h3>Company:</h3><h2>GSAC</h2>< br>
+                <h3>position:</h3><h2>Software Programmer</h2>< br>';
+    })->name('work-experience');
 
-Route::view('/', 'welcome');
+    Route::redirect('/', '/user/profile')->name('home');
 
-Route::get('/home', function () {
-    return to_route('admin.users.management');
 });
