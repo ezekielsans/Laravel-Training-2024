ec11cbfcd99b0cca8ff38b4226883e4a2bbea77d
Danniel Libor <dlibor.dev@gmail.com>
2024-07-28 15:12:57 +0800

    query builder

--- app/Http/Controllers/WorkExperienceController.php
+++ app/Http/Controllers/WorkExperienceController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\WorkExperience\StoreRequest;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class WorkExperienceController extends Controller
 {
@@ -17,6 +18,32 @@ class WorkExperienceController extends Controller
         $formData = $request->validated();
 
         // saving....
+
+        // $experiences = DB::select('SELECT * FROM work_experiences LIMIT 1');
+        $experiences = DB::table('work_experiences')
+            ->where('id', 1)
+            ->orderBy('company', 'asc')
+            ->get();
+
+        // ->chunk(1000, function ($works) {
+        //
+        // });
+        // ->exists();
+        // ->groupBy(['company', 'is_current'])
+        // ->selectRaw('COUNT(company) as total')
+        // ->selectRaw('company as name')
+        // ->whereRaw('id = ?', [2])
+        // ->find(1);
+        // ->where('id', 1)
+        // ->first();
+        // ->toSql();
+        // ->get();
+        // ->where('company', '=', 'Kshlerin-Ullrich')
+        // ->whereNotLike('')
+        // ->whereLike('company', '%Kshlerin-Ullrich%')
+        // ->where('company', 'like', '%Kshlerin-Ullrich%');
+
+        dd($experiences);
 
         return back()->with('success', 'Work Experience added successfully');
     }
